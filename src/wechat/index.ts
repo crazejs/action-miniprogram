00d341d5b3aafa63d11b90b3304417850c2288ae
yangyue <yangyue@wefind.ai>
2024-04-28 20:48:54 +0800

    chore: 我真是服了

--- src/wechat/index.ts
+++ src/wechat/index.ts
@@ -1,8 +1,5 @@
-import {
-  join,
-  // resolve
-} from 'node:path';
-import { existsSync } from 'node:fs';
+// import { join, resolve } from 'node:path';
+// import { existsSync } from 'node:fs';
 // import { cpus } from 'node:os';
 // import { upload, Project } from 'miniprogram-ci';
 
@@ -13,18 +10,16 @@ export interface UploadProps {
   privateKey: string;
 }
 
-export async function uploadWechatMiniProgram({
-  root,
-  // version, description, privateKey
-}: UploadProps): Promise<void> {
-  const projectPath = join(process.env.GITHUB_WORKSPACE ?? '', root);
-  const projectConfigPath = join(projectPath, 'project.config.json');
-  console.log(`[uploadWechatMiniProgram#projectConfigPath]`, projectConfigPath);
-
-  if (!existsSync(projectConfigPath)) {
-    throw new Error('project.config.json not found');
-  }
-
+export async function uploadWechatMiniProgram({ root, version, description, privateKey }: UploadProps): Promise<void> {
+  console.log('[uploadWechatMiniProgram]', root, version, description, privateKey);
+  // const projectPath = join(process.env.GITHUB_WORKSPACE ?? '', root);
+  // const projectConfigPath = join(projectPath, 'project.config.json');
+  // console.log(`[uploadWechatMiniProgram#projectConfigPath]`, projectConfigPath);
+  //
+  // if (!existsSync(projectConfigPath)) {
+  //   throw new Error('project.config.json not found');
+  // }
+  //
   // const projectConfig = await import(projectConfigPath);
   // console.log(`[uploadWechatMiniProgram#projectConfig]`, projectConfig);
   //
